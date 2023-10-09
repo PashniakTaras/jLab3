cc3df58b969c47c8967cb50123dc35a58f22db1d
Taras Pashniak <taraspashnyak93@gmail.com>
2023-10-09 12:37:03 +0300

    done2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean Continue = true;
+        boolean Continue = true; //t
         do {
             Scanner in = new Scanner(System.in);
             System.out.print("Лаб 2.\nВиберіть номер завдання (1-3): ");
